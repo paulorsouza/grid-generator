c111545f0958e6a1b004633e8e49ea49f4360010
Paulo Ricardo <paulor1809@gmail.com>
2017-12-12 19:12:44 -0200

    Init

---
+++ README.md
@@ -0,0 +1,15 @@
+# Grid Generator
+
+A plop sample that generates columns definition based on keys of a response.
+
+### Usage:
+
+```sh
+$ yarn
+$ yarn plop grid
+```
+
+### Prompt
+
+- url -> define endpoint base to columns
+- modelName -> name of folder that will be generated
---
+++ plopfile.js
@@ -0,0 +1,63 @@
+const fetch = require('node-fetch');
+const getKeys = require('object-keys');
+
+const template = 'import { Filters } from \'react-data-grid-addons\';\n\n' + 
+  'const columns = [\n' +
+  '{{#each names}}' +
+  '  {\n' +
+  '    key: \'{{this}}\',\n' +
+  '    name: \'{{titleCase this}}\',\n' +
+  '    filterable: true,\n' +
+  '    resizable: true,\n' +
+  '    hidden: false,\n' +
+  '    filterRenderer: Filters.AutoCompleteFilter,\n' +
+  '    sortable: true\n' +
+  '  },\n' +
+  '{{/each}}' +
+  '];\n' +
+  '\n' +
+  'export default columns;';
+
+module.exports = function (plop) {
+  plop.setHelper('getColumns', (url) => {
+    return JSON.stringify(columns)
+  });
+
+  plop.setGenerator('grid', {
+    description: 'this is a grid container generator',
+    prompts: [{
+      type: 'input',
+      name: 'url',
+      message: 'endpoint url'
+    }, {
+      type: 'input',
+      name: 'modelName',
+      message: 'file\'s name'
+    }],
+    actions: [{
+      type: 'add',
+      path: 'src/{{modelName}}/Grid{{properCase modelName}}.js',
+      templateFile: 'plop_templates/gridComponent.hbs'
+    }, {
+      type: 'add',
+      path: 'src/redux/modules/{{modelName}}.js',
+      templateFile: 'plop_templates/gridRedux.hbs'
+    }, function customAction(answers) {
+      process.chdir(plop.getPlopfilePath());
+      
+      const changeFilePath = 'src/' + answers.modelName + '/columns.js'
+      const fs = require('fs');
+      
+      fetch(answers.url)
+      .then(response => response.json())
+      .then(data => {
+        const keys = getKeys(data);
+        const obj = {names: keys}
+        const render = plop.renderString(template, obj);
+        fs.writeFileSync(changeFilePath, render);
+      });
+
+      return '';
+    }
+  ]});
+}
